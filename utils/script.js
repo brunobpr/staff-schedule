c15749ca8f8fa27505d5df6b731826193c22ebcf
Bruno Ribeiro <brunopr.bpr@gmail.com>
2021-02-20 20:21:41 +0000

    Implemented send roster by email function

--- utils/script.js
+++ utils/script.js
@@ -1,3 +1,4 @@
+
 function saveValue(text){
     savedvalue = text;
     hiddenInput = document.getElementsByName('staffName');
@@ -10,3 +11,35 @@ function saveValue(text){
     title = document.getElementById('modalTitle');
     title.textContent = "Select " +name + "'s " + day + " shift.";
 }
+
+
+function sendEmail(node) { 
+    
+    domtoimage.toPng(node).then(function (dataUrl) {    
+        var img = new Image();
+        img.src = dataUrl;
+        var emailSubject = (document.getElementById('week-title').textContent).toString(); 
+        emailList = document.getElementById('emailList').value;
+        console.log(emailList)
+        Email.send({ 
+              SecureToken: "6057518e-51be-47c4-8f4e-1ca615030a02",
+              To: "bprweb@aol.com", 
+              From: "brunopr.bpr@gmail.com", 
+              Subject: emailSubject, 
+              Body: "<h2>Roster for " + emailSubject + "</h2>", 
+              Attachments : [
+                {
+                    "type": "image/jpg", 
+                    "name": emailSubject +".jpg", 
+                    "data": dataUrl
+                }]
+            }) 
+              .then(function (message) { 
+                alert(message) 
+          }); 
+    }).catch(function (error) {
+        console.error('oops, something went wrong!', error);
+    });
+} 
+
+

632598614110fac404cae44814b3d0a6324e3610
Bruno Ribeiro <brunopr.bpr@gmail.com>
2022-03-18 19:46:34 +0000

    fixed day of the weeks

--- utils/date.js
+++ utils/date.js
@@ -1,13 +1,13 @@
 exports.getDays = function(weekNumber, yearNumber){
-    var adjust = 2021 - yearNumber;
+    var adjust = 2022 - yearNumber;
     return[
-        new Date(yearNumber, 0,(weekNumber*7) - 3 + adjust).toString().substring(0, 3) + " (" + new Date(yearNumber, 0,(weekNumber*7) - 4 + adjust).toString().substring(8, 10)+")",
-        new Date(yearNumber, 0,(weekNumber*7) - 2 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) - 3 + adjust).toString().substring(8, 10)+")",
-        new Date(yearNumber, 0,(weekNumber*7) - 1 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) - 2 + adjust).toString().substring(8, 10)+")",
-        new Date(yearNumber, 0,(weekNumber*7)     + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) - 1  + adjust).toString().substring(8, 10)+")",
-        new Date(yearNumber, 0,(weekNumber*7) + 1 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) + adjust).toString().substring(8, 10)+")",
-        new Date(yearNumber, 0,(weekNumber*7) + 2 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) + 1 + adjust).toString().substring(8, 10)+")",
-        new Date(yearNumber, 0,(weekNumber*7) + 3 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) + 2 + adjust).toString().substring(8, 10)+")",
+        new Date(yearNumber, 0,(weekNumber*7) - 4 + adjust).toString().substring(0, 3) + " (" + new Date(yearNumber, 0,(weekNumber*7) - 4 + adjust).toString().substring(8, 10)+")",
+        new Date(yearNumber, 0,(weekNumber*7) - 3 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) - 3 + adjust).toString().substring(8, 10)+")",
+        new Date(yearNumber, 0,(weekNumber*7) - 2 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) - 2 + adjust).toString().substring(8, 10)+")",
+        new Date(yearNumber, 0,(weekNumber*7) - 1 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) - 1  + adjust).toString().substring(8, 10)+")",
+        new Date(yearNumber, 0,(weekNumber*7) + 0 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) + adjust).toString().substring(8, 10)+")",
+        new Date(yearNumber, 0,(weekNumber*7) + 1 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) + 1 + adjust).toString().substring(8, 10)+")",
+        new Date(yearNumber, 0,(weekNumber*7) + 2 + adjust).toString().substring(0, 3) + " ("  + new Date(yearNumber, 0,(weekNumber*7) + 2 + adjust).toString().substring(8, 10)+")",
 ]
 
 }
